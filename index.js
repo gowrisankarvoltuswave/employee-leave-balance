610a26af99b55eea3cc22b2e80f9f7e32b0126a9
Gowrisankar <gowrisankar.maganti@voltuswave.com>
2018-03-19 12:57:39 +0530

    created test function

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/test',(req,res)=> res.send('You are awesome Man !'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/test', (req, res) => {
+    function callFirstFn() {
+      console.log(req)
+      res.send('You are awesome Man !')
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
